3d5cc82382479914e630fa1bbc8df627fc1d3271
Walter McKelvie <walt@mckelvie.org>
2023-07-08 12:07:43 -0400

    Removed env

--- env/lib/python3.9/site-packages/jupyter_matlab_kernel/__main__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2023 The MathWorks, Inc.
-# Use ipykernel infrastructure to launch the MATLAB Kernel.
-
-if __name__ == "__main__":
-    from ipykernel.kernelapp import IPKernelApp
-    from jupyter_matlab_kernel.kernel import MATLABKernel
-
-    IPKernelApp.launch_instance(kernel_class=MATLABKernel)
